7127b6b59461cee6d5ffc2316d76bd5eeac4544c
vasu <bandaruvasu1@gmail.com>
2022-02-09 20:59:55 +0530

    play book2

--- create-instance.sh
+++ create-instance.sh
@@ -2,8 +2,8 @@
 
 UPDATE_DNS_RECORDS() {
    IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
-  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/$(IP)/" record.jsm >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z10262683C43F5P8H0WOJ --change-batch file:///tmp/record.json | jq
+  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/$(IP)/" Record.json >/tmp/Record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z10262683C43F5P8H0WOJ --change-batch file:///tmp/Record.json | jq
 }
 CREATE() {
 count=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
